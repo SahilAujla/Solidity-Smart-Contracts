16ed716842748f899f1f26a7555d5df34bfe8c95
Sahil Aujla <sahilaujla15@gmail.com>
2022-02-22 20:14:49 +0530

    Create 3_Data_Types_1.sol

---
+++ 3_Data_Types_1.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
+
+contract MyContract {
+    uint public constant myUint = 1;
+    int public myInt = -1;
+    string public myString = "Sahil";
+    bool public mybool = true;
+
+    // some of the data types
+}
